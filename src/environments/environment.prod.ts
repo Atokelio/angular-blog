f6f609a9ee0479b0d7bc8e1825a8920b9e7662a5
Atokelio <saf-boris@yandex.ru>
2021-02-04 17:36:05 +0300

    add changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBuBBr1w-yFuqPghaDdfjacddmkEe0OtQs',
+  fbDbUrl: 'https://ng-blog-6d557-default-rtdb.firebaseio.com/'
+};
